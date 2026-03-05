13d137dc327f294c47e056a17cc8ea18e4727676
Yuriy <germanovich4job@gmail.com>
2026-03-05 22:58:42 +0300

    e

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,39 +1,30 @@
 import { Avatar, Button, Link } from "@mui/material";
 import { useGetCurrentUserQuery } from "../services";
 import { Login } from "@mui/icons-material";
-
 import { useNavigate } from "@tanstack/react-router";
 
 export const Profile = () => {
   const { data } = useGetCurrentUserQuery();
   const navigate = useNavigate();
 
-  const goToMe = () => navigate("/me" as any);
-
   return (
-    <>
-      <div
-        className="flex flex-row"
-        style={{ display: "flex", alignItems: "center", gap: 8 }}
-      >
-        {data?.username ? (
-          <>
-            <Link href="/me">
-              <Avatar
-                alt={data?.username || ""}
-                src={data?.avatar}
-                onClick={goToMe}
-                style={{ cursor: "pointer" }}
-                sx={{ width: 30, height: 30 }}
-              />
-            </Link>
-          </>
-        ) : (
-          <Button color="primary" href="/auth/login">
-            <Login />
-          </Button>
-        )}
-      </div>
-    </>
+    <div className="flex items-center gap-2">
+      {data?.username ? (
+        <>
+          <Link href="/me" className="no-underline">
+            <Avatar
+              alt={data?.username || ""}
+              src={data?.avatar}
+              onClick={() => navigate({ to: "/me" })}
+              className="cursor-pointer scale-80"
+            />
+          </Link>
+        </>
+      ) : (
+        <Button color="primary" href="/auth/login">
+          <Login />
+        </Button>
+      )}
+    </div>
   );
 };
